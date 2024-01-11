e22564840625eb21b5f68dc994a3a97095e15756
Santiago Crippa <santicrippa@gmail.com>
2024-01-11 00:32:22 -0300

    navbar mockup

--- src/components/navbar/items/NavItems.tsx
+++ src/components/navbar/items/NavItems.tsx
@@ -3,21 +3,23 @@ import Link from "next/link"
 import { Items } from "./navitems.types"
 
 export const NavItems = () => {
+
   const items: Items[] = [
     { name: "Home", path: "/" },
     { name: "Works", path: "/works" },
     { name: "About", path: "/about" },
     { name: "Contact", path: "/contact" }
   ]
+
   return (
     <>
-      <div className="text-light">
+      <div className="w-full flex justify-center gap-3 text-light border">
         {items.map(item => (
           <ul key={item.name}>
-            <Link href={item.path}>{item.name}</Link>
+            <Link href={item.path} className="text-sm">{item.name}</Link>
           </ul>
         ))}
       </div>
     </>
   )
-}
+};
