c94d4f612ffbfcd3fdc2917907047c6fad91b9b9
Santiago Crippa <santicrippa@gmail.com>
2024-03-15 22:46:47 -0300

    fixing project reponsive

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -24,10 +24,10 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
         scale: scaleProgress,
         opacity: opacityProgress,
       }}
-      className="group mb-3 sm:mb-8 last:mb-0"
+      className="group mb-3 px-2 md:px-0 sm:mb-8 last:mb-0"
     >
-      <section className='relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden rounded-lg sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8'>
-        <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
+      <section className='relative bg-gray-100 flex flex-col max-w-sm md:max-w-[42rem] border border-black/5 overflow-hidden rounded-lg sm:pr-8 sm:h-[20rem] hover:bg-gray-200 transition md:group-even:pl-8 group-hover:border-none'>
+        <div className='pt-4 pb-7 px-5  sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full md:group-even:ml-[18rem]'>
           <h3 className='text-2xl font-semibold'>{title}</h3>
           <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
           <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -41,19 +41,25 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
           </ul>
         </div>
 
-        <Image src={imageUrl} alt={`My project image: ${title}`} quality={95} className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition h-full object-cover
-        group-hover:scale-[1.04] 
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
+        <Image
+          src={imageUrl}
+          alt={`My project image: ${title}`}
+          quality={95}
+          className='md:absolute md:top-8 md:-right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition h-full object-cover
 
-        group-even:right-[initial] 
-        group-even:-left-40 
+          group-hover:scale-110
+          md:group-hover:scale-[1.04] 
+          md:group-hover:-translate-x-3
+          md:group-hover:translate-y-3
+          md:group-hover:-rotate-2
 
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2
-      '/>
+          md:group-even:right-[initial] 
+          md:group-even:-left-40 
+
+          md:group-even:group-hover:translate-x-3
+          md:group-even:group-hover:translate-y-3
+          md:group-even:group-hover:rotate-2
+        '/>
       </section>
     </motion.div>
 
